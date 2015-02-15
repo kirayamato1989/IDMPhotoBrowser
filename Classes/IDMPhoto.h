af4b4b8b150cf1e6d03b75301fcae048290de9e5
mythodeia <mythodeia@users.noreply.github.com>
2015-02-15 17:49:26 +0200

    use SDWebImage instead of AFNetworking

--- Classes/IDMPhoto.h
+++ Classes/IDMPhoto.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "IDMPhotoProtocol.h"
-#import "AFNetworking.h"
+#import "UIImageView+WebCache.h"
 
 // This class models a photo/image and it's caption
 // If you want to handle photos, caching, decompression
